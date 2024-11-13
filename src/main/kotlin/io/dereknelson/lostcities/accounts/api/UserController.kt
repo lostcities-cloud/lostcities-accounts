0b2486f02fde3fcc879f25edffc1350a40210a99
nelson54 <me@dereknelson.io>
2024-11-13 13:16:38 -0500

    ktlint format

--- src/main/kotlin/io/dereknelson/lostcities/accounts/api/UserController.kt
+++ src/main/kotlin/io/dereknelson/lostcities/accounts/api/UserController.kt
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
 import org.modelmapper.ModelMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -27,7 +28,6 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
-import jakarta.validation.Valid
 
 @Tag(name = "User actions")
 @RestController
