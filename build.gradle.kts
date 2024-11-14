53ecb387b29e56e73516004234b8a3c4bd3661a1
nelson54 <me@dereknelson.io>
2024-11-14 14:15:04 -0500

    Fixing null pointer exception creating ai turns.

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,7 +106,7 @@ dependencies {
         implementation(project(":lostcities-models"))
     } else {
         implementation("io.dereknelson.lostcities-cloud:lostcities-common:0.0.5")
-        implementation("io.dereknelson.lostcities-cloud:lostcities-models:0.0.5")
+        implementation("io.dereknelson.lostcities-cloud:lostcities-models:0.0.6")
     }
 
 	implementation("org.apache.commons:commons-lang3:3.12.0")
